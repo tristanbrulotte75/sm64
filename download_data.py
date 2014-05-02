0158afb8ed1577ea9e40ffc83d451e6a36c462e9
Robert Davis <rtdavis22@gmail.com>
2014-05-02 04:09:25 -0700

    Update download script.

--- download_data.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-url_base = 'https://googledrive.com/host/0B_WJBlETj-zLN0lNRUpaQzg0WGs/'
-
-for i in range(121):
-    subprocess.call(['wget', '-nc', '-P', 'data', url_base + 'star_%s.dat' % i])
---
+++ download_data.sh
@@ -0,0 +1,3 @@
+wget https://googledrive.com/host/0B_WJBlETj-zLSDE2cjFqUHk3QlU/stars.tar.gz
+mkdir data
+tar -zxf stars.tar.gz -C data
